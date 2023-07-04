fc219c1ca7afeaf9df17ed4ff6752f20a5af2f18
V M <vineeth.markonda@wipro.com>
2023-07-04 17:16:47 +1000

    sec finish for now

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,47 @@ import Link from 'next/link';
 import NavBar from './components/NavBar';
 import Header from './components/Header';
 import RestaurantCard from './components/RestaurantCard';
+import { PrismaClient, Cuisine, Location, PRICE } from '@prisma/client';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+const prisma = new PrismaClient();
+
+export interface RestaurantCardType {
+  id: number;
+  slug: string;
+  name: string;
+  main_image: string;
+  cuisine: Cuisine;
+  location: Location;
+  price: PRICE;
+}
+
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      slug: true,
+      name: true,
+      main_image: true,
+      cuisine: true,
+      location: true,
+      price: true,
+    }
+  });
+  return restaurants;
+}
+
+export default async function Home() {
+  const restaurants = await fetchRestaurants();
   return (
-        <main>
-          <Header />
-          <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-            <RestaurantCard />
-          </div>
-        </main>
+    <main>
+      <Header />
+      <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+        {restaurants.map(restaurant => (
+          <RestaurantCard restaurant={restaurant} />
+        ))}
+      </div>
+    </main>
   )
 }
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { PrismaClient } from '@prisma/client';
 import Link from 'next/link';
 import NavBar from '../../components/NavBar';
 import Description from './components/Description';
@@ -9,15 +10,49 @@ import RestaurantNavBar from './components/RestaurantNavBar';
 import Reviews from './components/Reviews';
 import Title from './components/Title';
 
-export default function RestaurantDetails() {
+const prisma = new PrismaClient();
+
+interface Restaurant {
+    id: number;
+    name: string;
+    images: string[];
+    description: string;
+    slug: string;
+}
+
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+    const restaurant = await prisma.restaurant.findUnique({
+        where: {
+            slug
+        }, 
+        select: {
+            id: true,
+            name: true,
+            images: true,
+            description: true,
+            slug: true,
+        }
+    })
+
+    if (!restaurant) {
+        throw new Error();
+    }
+    return restaurant;
+}
+
+export default async function RestaurantDetails({params}: {params: {slug: string}}) {
+    
+    const restaurant = await fetchRestaurantBySlug(params.slug);
+    console.log(restaurant)
+
     return (
         <>
                 <div className="bg-white w-[70%] rounded p-3 shadow">
-                    <RestaurantNavBar />
-                    <Title />
+                    <RestaurantNavBar slug={restaurant.slug}/>
+                    <Title name={restaurant.name}/>
                     <Rating />
-                    <Description />
-                    <Images />
+                    <Description description={restaurant.description}/>
+                    <Images images={restaurant.images}/>
                     <Reviews />
                 </div>
                 <div className="w-[27%] relative text-reg">
